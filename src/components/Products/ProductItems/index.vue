685ed62a9e2d47fdd18067a0f0727cc9a8bdcad3
miquelven <miquelven.silva@gmail.com>
2024-03-29 09:26:13 -0300

    melhoria no carousel

--- src/components/Products/ProductItems/index.vue
+++ src/components/Products/ProductItems/index.vue
@@ -1,78 +1,17 @@
-<script setup>
-import { ref } from "vue";
-import Container from "../../Container/index.vue";
-
-const model = ref(null);
-
-const data = [
-  {
-    products: [
-      {
-        img: "../../../../public/images/products/camisa.png",
-        name: "Camisa Branca",
-        totalPrice: 80,
-        price: 40,
-        discount: 50,
-      },
-      {
-        img: "../../../../public/images/products/calça-preta.jpg",
-        name: "Calça Esportiva",
-        totalPrice: 50,
-        price: 50,
-        discount: 0,
-      },
-      {
-        img: "../../../../public/images/products/moletom-preto.png",
-        name: "Moletom ",
-        totalPrice: 140,
-        price: 100,
-        discount: 30,
-      },
-      {
-        img: "../../../../public/images/products/calça-camuflada.png",
-        name: "Calça Camuflada",
-        totalPrice: 120,
-        price: 120,
-        discount: 0,
-      },
-    ],
-  },
-  {
-    products: [
-      {
-        img: "../../../../public/images/products/short.png",
-        name: "Short",
-        totalPrice: 30,
-        price: 20,
-        discount: 35,
-      },
-      {
-        img: "../../../../public/images/products/moletom.png",
-        name: "Moletom",
-        totalPrice: 100,
-        price: 80,
-        discount: 20,
-      },
-    ],
-  },
-];
-</script>
-
 <template>
-  <div class="relative mt-20">
-    <!-- <Container> -->
-    <v-slide-group v-model="model" show-arrows class="relative z-10">
-      <v-slide-group-item
-        v-for="(product, index) in data"
-        :key="index"
-        v-slot="{ selectedClass }"
-      >
+  <Container>
+    <Carousel
+      v-bind="settings"
+      :breakpoints="breakpoints"
+      :wrapAround="true"
+      :transition="500"
+      :autoplay="4000"
+      class="flex flex-col gap-5 w-full"
+    >
+      <Slide v-for="(product, index) in data.products" :key="index">
         <v-card
-          :class="['ma-4', selectedClass]"
-          class="relative pt-10 w-[320px] h-[384px] max-lg:w-[280px] max-lg:h-[344px] max-md:w-[220px] max-md:h-[284px] max-sm:w-[150px] max-sm:h-[300px]"
-          v-for="(items, itemIndex) in product.products"
-          :key="itemIndex"
-          @click="toggle"
+          elevated="0"
+          class="relative pt-10 w-[320px] h-[384px] max-lg:w-[280px] max-lg:h-[344px] max-md:w-[220px] max-md:h-[284px]"
         >
           <!-- whatsapp button -->
           <v-btn
@@ -90,19 +29,19 @@ const data = [
           <!-- discount button -->
 
           <v-card
-            v-if="items.discount"
+            v-if="product.discount"
             color="#D77A61"
             class="w-14 h-7 text-white flex justify-center items-center max-md:w-11 max-md:h-7"
             style="position: absolute; top: 0; right: 0"
           >
             <span
               class="text-center w-full h-full inline-block font-semibold max-md:text-xs"
-              >{{ items.discount }}%</span
+              >{{ product.discount }}%</span
             >
           </v-card>
 
           <img
-            :src="items.img"
+            :src="product.img"
             alt="imagem do produto"
             class="h-[242px] flex-1 object-cover object-center mx-auto max-lg:h-[200px] max-md:h-[150px] max-sm:h-[120px] max-sm:mt-5"
           />
@@ -110,20 +49,120 @@ const data = [
             <h4
               class="text-center text-xl font-semibold max-lg:text-lg max-md:text-base"
             >
-              {{ items.name }}
+              {{ product.name }}
             </h4>
             <div
               class="flex gap-3 justify-center items-center font-medium max-lg:text-sm max-md:text-xs"
             >
-              <span> R${{ items.price }},00 </span>
+              <span> R${{ product.price }},00 </span>
 
-              <span v-if="items.price !== items.totalPrice" class="text-[#aaa]">
-                R${{ items.totalPrice }},00
+              <span
+                v-if="product.price !== product.totalPrice"
+                class="text-[#aaa]"
+              >
+                R${{ product.totalPrice }},00
               </span>
             </div>
           </div>
         </v-card>
-      </v-slide-group-item>
-    </v-slide-group>
-  </div>
+      </Slide>
+
+      <template #addons>
+        <Navigation />
+        <Pagination />
+      </template>
+    </Carousel>
+  </Container>
 </template>
+
+<script>
+import { defineComponent } from "vue";
+import { Carousel, Navigation, Slide, Pagination } from "vue3-carousel";
+import Container from "../../Container/index.vue";
+
+import "vue3-carousel/dist/carousel.css";
+
+export default defineComponent({
+  name: "Breakpoints",
+  components: {
+    Carousel,
+    Slide,
+    Navigation,
+    Pagination,
+    Container,
+  },
+  data: () => ({
+    // carousel settings
+    settings: {
+      itemsToShow: 1,
+      snapAlign: "center",
+    },
+    // breakpoints are mobile first
+    // any settings not specified will fallback to the carousel settings
+    breakpoints: {
+      // 700px and up
+      500: {
+        itemsToShow: 2,
+        snapAlign: "center",
+      },
+      // 1024 and up
+      1024: {
+        itemsToShow: 3,
+        snapAlign: "center",
+      },
+    },
+    data: {
+      products: [
+        {
+          img: "../../../../public/images/products/camisa.png",
+          name: "Camisa Branca",
+          totalPrice: 80,
+          price: 40,
+          discount: 50,
+        },
+        {
+          img: "../../../../public/images/products/calça-preta.jpg",
+          name: "Calça Esportiva",
+          totalPrice: 50,
+          price: 50,
+          discount: 0,
+        },
+        {
+          img: "../../../../public/images/products/moletom-preto.png",
+          name: "Moletom ",
+          totalPrice: 140,
+          price: 100,
+          discount: 30,
+        },
+        {
+          img: "../../../../public/images/products/calça-camuflada.png",
+          name: "Calça Camuflada",
+          totalPrice: 120,
+          price: 120,
+          discount: 0,
+        },
+        {
+          img: "../../../../public/images/products/short.png",
+          name: "Short",
+          totalPrice: 30,
+          price: 20,
+          discount: 35,
+        },
+        {
+          img: "../../../../public/images/products/moletom.png",
+          name: "Moletom",
+          totalPrice: 100,
+          price: 80,
+          discount: 20,
+        },
+      ],
+    },
+  }),
+});
+</script>
+
+<style>
+.v-card--variant-elevated {
+  box-shadow: none !important;
+}
+</style>
--- src/components/Products/index.vue
+++ src/components/Products/index.vue
@@ -4,8 +4,8 @@ import ProductItems from "./ProductItems/index.vue";
 </script>
 <template>
   <section class="my-80">
-    <Container>
-      <h3 class="text-4xl font-bold mb-5 max-lg:text-3xl max-sm:text-2xl">
+    <!-- <Container> -->
+    <!-- <h3 class="text-4xl font-bold mb-5 max-lg:text-3xl max-sm:text-2xl">
         Alguns Produtos
       </h3>
 
@@ -14,11 +14,11 @@ import ProductItems from "./ProductItems/index.vue";
           >Mais produtos</span
         >
 
-        <!-- line -->
+         line 
         <div class="absolute -bottom-1 left-0 w-full h-0.5 bg-[#eee]"></div>
-      </div>
+      </div> -->
 
-      <ProductItems />
-    </Container>
+    <ProductItems />
+    <!-- </Container> -->
   </section>
 </template>
